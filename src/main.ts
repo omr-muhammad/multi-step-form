5de7439bd89d40d26b04ca483e6c0c03567bde2b
unknown <omarelsayedmuhammad@gmail.com>
2024-07-20 15:44:49 +0300

    finished step three UI

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./styles/style.scss";
+
+// import { stepOne, stepTwo, stepsHeadings } from "./ts/htmlTemplates";
+
+/* ------- Global Variables ------- */
+
+// Elements
+const stepTitle = document.querySelector(
+  "[data-step-title]"
+) as HTMLHeadingElement;
+
+const stepSubtitle = document.querySelector(
+  "[data-step-subtitle]"
+) as HTMLHeadingElement;
+
+const currentStepContent = document.querySelector(
+  "[data-step-content]"
+) as HTMLDivElement;
+
+// HTML
